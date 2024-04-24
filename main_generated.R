d17780777e16d35e010d980cb00f5ea9de148205
Gloria <gpietrop@Glorias-MacBook-Air.local>
2024-04-24 10:12:14 +0100

    fix parameters for external users run.

--- main_generated.R
+++ main_generated.R
@@ -7,7 +7,7 @@ source("hyperparameters.R")
 
 # Parse options with optparse
 option_list <- list(
-  make_option(c("--popSize"), type="integer", default=100, help="Population size"),
+  make_option(c("--popSize"), type="integer", default=1000, help="Population size"),
   make_option(c("--maxiter"), type="integer", default=100, help="Maximum number of iterations"),
   make_option(c("--pmutation"), type="double", default=1.0, help="Mutation rate"),
   make_option(c("--pcrossover"), type="double", default=0.8, help="Crossover rate"),

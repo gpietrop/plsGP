2cd00415da0473ae4751d50d073d589e5e564681
gpietrop <gloria.pietropolli@gmail.com>
2024-09-29 08:55:49 +0200

    add .sh files for final(?) exp.

---
+++ run_str3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Initialize Conda
+eval "$(conda shell.bash hook)"
+conda activate plsGP
+
+# Array of models and dimensions
+models=("str3_small" "str3_med" "str3_high")
+dimensions=(100 250 500)
+pop=100
+ep=200
+end=199
+
+# Run R scripts in parallel
+for model in "${models[@]}"; do
+    for dim in "${dimensions[@]}"; do
+        echo "Running Rscript for model $model with modeDim $dim"
+        Rscript main_timing.R --model=$model --modeDim=$dim --popSize=$pop --maxiter=$ep --seed_end=$end &
+    done
+done
+
+# Wait for all background processes to finish
+wait
+
+echo "All tasks have been launched."
+

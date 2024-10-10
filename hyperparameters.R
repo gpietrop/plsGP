d7a10f868b559df63784f836859193d8608a5e76
gpietrop <gloria.pietropolli@gmail.com>
2024-10-10 15:06:22 +0200

    change str2 coefficients.

--- hyperparameters.R
+++ hyperparameters.R
@@ -165,9 +165,9 @@ str2_med <- '
   y17 ~~ 0.16*y18
   
   # Structural model
-  eta4 ~ 0.38*eta1 + 0.35*eta2 + 0.3*eta3
-  eta5 ~ 0.45*eta4 + 0.4*eta3
-  eta6 ~ 0.4*eta5 + 0.35*eta1
+  eta4 ~ 0.35*eta1 + 0.3*eta2 + 0.32*eta3
+  eta5 ~ 0.4*eta4 + 0.36*eta3
+  eta6 ~ 0.34*eta5 + 0.38*eta1
   eta1 ~~ 0.3*eta2 + 0.5*eta3   
   eta2 ~~ 0.4*eta3
 '
@@ -196,12 +196,13 @@ str2_high <- '
   y17 ~~ 0.16*y18
   
   # Structural model
-  eta4 ~ 0.3*eta1 + 0.5*eta2 + 0.4*eta3
-  eta5 ~ 0.8*eta4 + 0.23*eta3
-  eta6 ~ 0.5*eta5 + 0.6*eta1
+  eta4 ~ 0.36*eta1 + 0.38*eta2 + 0.37*eta3
+  eta5 ~ 0.44*eta4 + 0.48*eta3
+  eta6 ~ 0.45*eta5 + 0.5*eta1
   eta1 ~~ 0.3*eta2 + 0.5*eta3   
   eta2 ~~ 0.4*eta3
 '
+
 
 str3_small <- '
 
